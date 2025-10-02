fdc3145cc939633be46e172bc55f208ebe834225
Marcos2593 <mromjim783@g.educaand.es>
2025-10-02 19:42:12 +0200

    Actualizar versión y añadir info de funcionalidades

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,7 @@
 def main():
-    print("Calculadora v1.0")
+    print("Calculadora v1.1")
     print("Desarrollada por: Alumno A y Alumno B")
+    print("Funcionalidades: Operaciones básicas")
 
 if __name__ == "__main__":
     main()
